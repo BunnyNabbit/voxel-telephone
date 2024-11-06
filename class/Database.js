d11446401749cb11803bb412e311ca2a90c896d0
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-05 21:28:55 -0600

    Initialize DB collections wizhin class

--- class/Database.js
+++ class/Database.js
@@ -1,19 +1,18 @@
 const mongojs = require("mongojs")
-const serverConfiguration = require("../config.json")
-const gameCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephone")
-const reportCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneReports")
-const interactionCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneInteractions")
-const portalCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephonePortals")
-const userCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneUsers")
-const banCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneBans")
-const ledgerCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneLedger")
-const purgedCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephonePurged")
 
 const Zone = require("./Zone.js")
 const UserRecord = require("./UserRecord.js")
 
 class Database {
 	constructor(serverConfiguration) {
+		this.gameCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephone")
+		this.reportCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneReports")
+		this.interactionCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneInteractions")
+		this.portalCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephonePortals")
+		this.userCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneUsers")
+		this.banCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneBans")
+		this.ledgerCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephoneLedger")
+		this.purgedCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephonePurged")
 		this.playerReserved = new Map()
 	}
 	findActiveGames(username, levels) {
@@ -22,7 +21,7 @@ class Database {
 			// find reserved game if it exists
 			const reserved = this.playerReserved.get(username)
 			if (reserved) return resolve([reserved])
-			gameCollection.find({ active: true }, async (err, docs) => {
+			this.gameCollection.find({ active: true }, async (err, docs) => {
 				if (err) return resolve([])
 				for (let i = 0; i < docs.length; i++) {
 					const game = docs[i]
@@ -44,7 +43,7 @@ class Database {
 
 	getGame(gameRootId) {
 		return new Promise(resolve => {
-			gameCollection.find({ root: gameRootId }).sort({ depth: 1 }, (err, games) => {
+			this.gameCollection.find({ root: gameRootId }).sort({ depth: 1 }, (err, games) => {
 				resolve(games)
 			})
 		})
@@ -58,7 +57,7 @@ class Database {
 			if (cursor) {
 				findDocument._id = { $lt: cursor }
 			}
-			gameCollection.find(findDocument).sort({ _id: -1 }).limit(limit, async (err, games) => {
+			this.gameCollection.find(findDocument).sort({ _id: -1 }).limit(limit, async (err, games) => {
 				let promises = []
 				games.forEach(game => {
 					promises.push(this.getGame(game._id))
@@ -70,7 +69,7 @@ class Database {
 
 	async getPortals(name) {
 		return new Promise(resolve => {
-			portalCollection.findOne({ _id: name }, (err, doc) => {
+			this.portalCollection.findOne({ _id: name }, (err, doc) => {
 				if (err || !doc) return resolve([])
 				const zones = doc.portals.map(zone => Zone.deserialize(zone))
 				resolve(zones)
@@ -81,7 +80,7 @@ class Database {
 	saveLevelPortals(level) {
 		return new Promise(resolve => {
 			const portals = level.portals.map(portal => portal.serialize())
-			portalCollection.replaceOne({ _id: level.name }, { _id: level.name, portals }, { upsert: true }, (err,) => {
+			this.portalCollection.replaceOne({ _id: level.name }, { _id: level.name, portals }, { upsert: true }, (err,) => {
 				if (err) console.log(err)
 				resolve()
 			})
@@ -103,7 +102,7 @@ class Database {
 				parent: "self",
 				depth: 0
 			}
-			gameCollection.insert(document, (err) => {
+			this.gameCollection.insert(document, (err) => {
 				resolve(document)
 			})
 		})
@@ -111,7 +110,7 @@ class Database {
 
 	getInteraction(username, id, type) {
 		return new Promise(resolve => {
-			interactionCollection.find({ username, forId: id, type }, (err, docs) => {
+			this.interactionCollection.find({ username, forId: id, type }, (err, docs) => {
 				if (err) return resolve(null)
 				resolve(docs[0])
 			})
@@ -120,7 +119,7 @@ class Database {
 
 	addReport(username, id, reason) {
 		return new Promise(resolve => {
-			reportCollection.insert({
+			this.reportCollection.insert({
 				username, forId: id, reason, unresolved: true
 			}, (err) => {
 				resolve()
@@ -130,7 +129,7 @@ class Database {
 
 	updateReportStatus(reportId, status) {
 		return new Promise(resolve => {
-			reportCollection.update({ _id: reportId }, { $set: { unresolved: status } }, (err) => {
+			this.reportCollection.update({ _id: reportId }, { $set: { unresolved: status } }, (err) => {
 				resolve()
 			})
 		})
@@ -138,7 +137,7 @@ class Database {
 
 	getReports(gameIds) {
 		return new Promise(resolve => {
-			reportCollection.find({ _id: { $in: gameIds } }, (err, reports) => {
+			this.reportCollection.find({ _id: { $in: gameIds } }, (err, reports) => {
 				resolve(reports)
 			})
 		})
@@ -146,7 +145,7 @@ class Database {
 
 	addInteraction(username, id, type) {
 		return new Promise(resolve => {
-			interactionCollection.insert({
+			this.interactionCollection.insert({
 				username, forId: id, type
 			}, (err) => {
 				resolve()
@@ -156,7 +155,7 @@ class Database {
 
 	deactivateGame(gameId) {
 		return new Promise(resolve => {
-			gameCollection.update({ _id: gameId }, { $set: { active: false } }, (err) => {
+			this.gameCollection.update({ _id: gameId }, { $set: { active: false } }, (err) => {
 				resolve()
 			})
 		})
@@ -164,7 +163,7 @@ class Database {
 
 	continueGame(originalDocument, newGameId, promptType, username, description) {
 		return new Promise(resolve => {
-			gameCollection.update({ _id: originalDocument._id }, { $set: { active: false } }, (err) => {
+			this.gameCollection.update({ _id: originalDocument._id }, { $set: { active: false } }, (err) => {
 				const gameNextId = new mongojs.ObjectID()
 				const document = {
 					_id: newGameId,
@@ -187,7 +186,7 @@ class Database {
 				} else {
 					document.promptType = "build"
 				}
-				gameCollection.insert(document, (err) => {
+				this.gameCollection.insert(document, (err) => {
 					if (!document.active) {
 						resolve({ document, status: 1 })
 					}
@@ -199,7 +198,7 @@ class Database {
 
 	getUserRecordDocument(username) {
 		return new Promise((resolve, reject) => {
-			userCollection.findOne({ _id: username }, (err, document) => {
+			this.userCollection.findOne({ _id: username }, (err, document) => {
 				if (err) return reject(err)
 				if (document) {
 					resolve(document)
@@ -212,7 +211,7 @@ class Database {
 
 	addTransaction(account, currency, amount) {
 		return new Promise((resolve) => {
-			ledgerCollection.insert({ account, currency, amount }, (err) => {
+			this.ledgerCollection.insert({ account, currency, amount }, (err) => {
 				resolve()
 			})
 		})
@@ -234,7 +233,7 @@ class Database {
 			}
 		]
 		return new Promise((resolve) => {
-			ledgerCollection.aggregate(aggregation, (err, docs) => {
+			this.ledgerCollection.aggregate(aggregation, (err, docs) => {
 				const map = new Map()
 				docs.forEach(doc => {
 					map.set(doc._id, doc.sum)
@@ -261,14 +260,14 @@ class Database {
 						turn.root = divergedRootId
 						turn.parent = "self"
 						turn.depth = newDepth
-						gameCollection.remove({ _id: oldId }, (err) => {
-							gameCollection.insert(turn, (err) => {
+						this.gameCollection.remove({ _id: oldId }, (err) => {
+							this.gameCollection.insert(turn, (err) => {
 								resolve()
 							})
 						})
 					} else {
 						const updateDocument = { $set: { depth: newDepth, parent: parentId, root: divergedRootId } }
-						gameCollection.update({ _id: turn._id }, updateDocument, (err) => {
+						this.gameCollection.update({ _id: turn._id }, updateDocument, (err) => {
 							resolve()
 						})
 					}
@@ -279,12 +278,12 @@ class Database {
 		})
 		await Promise.all(promises)
 		await this.regenerateGameNextTurnId(previousTurnId)
-		gameCollection.update({ _id: previousTurnId }, { $set: { active: true } })
+		this.gameCollection.update({ _id: previousTurnId }, { $set: { active: true } })
 	}
 
 	regenerateGameNextTurnId(gameId) {
 		return new Promise(resolve => {
-			gameCollection.update({ _id: gameId }, { $set: { next: new mongojs.ObjectID() } }, (err) => {
+			this.gameCollection.update({ _id: gameId }, { $set: { next: new mongojs.ObjectID() } }, (err) => {
 				resolve()
 			})
 		})
@@ -295,13 +294,13 @@ class Database {
 		const lastTurn = turns[turns.length - 1]
 		if (lastTurn.depth !== 0) {
 			const previousTurnId = turns[turns.length - 2]._id
-			gameCollection.update({ _id: previousTurnId }, { $set: { active: true } })
+			this.gameCollection.update({ _id: previousTurnId }, { $set: { active: true } })
 			await this.regenerateGameNextTurnId(previousTurnId)
 		}
 		lastTurn.reason = reason
-		purgedCollection.insert(lastTurn)
+		this.purgedCollection.insert(lastTurn)
 		return new Promise(resolve => {
-			gameCollection.remove({ _id: lastTurn._id }, (err) => {
+			this.gameCollection.remove({ _id: lastTurn._id }, (err) => {
 				resolve()
 			})
 		})
@@ -316,7 +315,7 @@ class Database {
 				acknowledged: false,
 				type: configuration.type
 			}
-			banCollection.insert(document, (err) => {
+			this.banCollection.insert(document, (err) => {
 				resolve()
 			})
 		})
@@ -335,19 +334,19 @@ class Database {
 			} else {
 				findDocument = { username }
 			}
-			banCollection.find(findDocument, (err, docs) => {
+			this.banCollection.find(findDocument, (err, docs) => {
 				resolve(docs)
 			})
 		})
 	}
 
 	acknowledgeBan(banId) {
-		banCollection.update({ _id: banId }, { $set: { acknowledged: true } })
+		this.banCollection.update({ _id: banId }, { $set: { acknowledged: true } })
 	}
 
 	saveUserRecord(userRecord) {
 		return new Promise(async resolve => {
-			userCollection.replaceOne({ _id: userRecord.username }, await userRecord.data, { upsert: true }, (err) => {
+			this.userCollection.replaceOne({ _id: userRecord.username }, await userRecord.data, { upsert: true }, (err) => {
 				this.draining = false
 				if (err) console.error(err)
 				resolve()
