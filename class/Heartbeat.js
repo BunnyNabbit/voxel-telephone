8fb49f7139a53dc2b9704c72cf421173f6c003a2
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-05 21:46:08 -0600

    Add git hash to heartbeat software name
    Also rename software to Voxel Telephone, as Classicborne Protocol is just zhe protocol library zhis server uses.

--- class/Heartbeat.js
+++ class/Heartbeat.js
@@ -1,6 +1,8 @@
 const qs = require("qs")
 const axios = require("axios")
 const crypto = require("crypto")
+const fs = require("fs")
+const { join } = require('node:path')
 
 class Heartbeat {
 	constructor(urlBase, universe) {
@@ -11,7 +13,9 @@ class Heartbeat {
 		setInterval(() => {
 			this.postHeartbeat()
 		}, 60000)
+		this.softwareName = `&9Voxel &3Telephone &7@ ${Heartbeat.getGitHash()}`.substring(0, 63)
 		this.postHeartbeat()
+		console.log(this.softwareName)
 	}
 	postHeartbeat() {
 		let clientCount = Math.min(this.universe.server.clients.length, 32)
@@ -22,7 +26,7 @@ class Heartbeat {
 			port: this.universe.serverConfiguration.port.toString(),
 			users: clientCount.toString(),
 			max: "64",
-			software: "Classicborne Protocol",
+			software: this.softwareName,
 			public: "true",
 			web: "true",
 			salt: this.salt.toString("hex"),
@@ -33,6 +37,15 @@ class Heartbeat {
 		}).catch((err) => {
 			console.log(err)
 		})
+	}
+	// https://stackoverflow.com/a/56975550
+	static getGitHash() {
+		const rev = fs.readFileSync(join(__dirname, "../.git/HEAD")).toString().trim().split(/.*[: ]/).slice(-1)[0]
+		if (rev.indexOf('/') === -1) {
+			return rev
+		} else {
+			return fs.readFileSync('.git/' + rev).toString().trim()
+		}
 	}
 }
 
