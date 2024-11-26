7898360146a135484a016e4db8964d51c71ff134
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-25 20:33:11 -0600

    Discord webhook integration

---
+++ class/integrations/DiscordWebhook.js
@@ -0,0 +1,31 @@
+const PushIntegration = require("./PushIntegration.js")
+
+class DiscordWebhook extends PushIntegration {
+	constructor(interests, authData, universe) {
+		super(interests, universe)
+		this.webhookUrl = authData.webhookUrl
+	}
+
+	async postMessage(text) {
+		return fetch(this.webhookUrl, {
+			"headers": {
+				"accept": "application/json",
+				"content-type": "application/json",
+			},
+			"body": JSON.stringify({
+				attachments: [],
+				content: "",
+				embeds: [
+					{
+						"type": "rich",
+						"description": text,
+						"content_scan_version": 0
+					}
+				]
+			}),
+			"method": "POST"
+		})
+	}
+}
+
+module.exports = DiscordWebhook
