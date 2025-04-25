eeb27c4a6b16de6fa377d13a9311520e8aaa5f08
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-04-24 22:50:14 -0500

    Enable prefetching

--- astro-website/astro.config.mjs
+++ astro-website/astro.config.mjs
@@ -8,5 +8,9 @@ export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
-  })
+  }),
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover"
+  }
 });
