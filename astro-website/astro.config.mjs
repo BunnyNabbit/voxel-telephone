0fae25bed6fb1d944b4a0294d9a228d282b6a7ad
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-05-07 00:38:17 -0500

    Add sitemap

--- astro-website/astro.config.mjs
+++ astro-website/astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
+import config from "../config.json"
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: config.website.baseURL,
   adapter: node({
     mode: 'standalone',
   }),
+  integrations: [sitemap()],
   prefetch: {
     defaultStrategy: "hover"
   }
