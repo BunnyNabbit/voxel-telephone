eb1cfbde05a53ab33b3181f8fcecd9c311aac072
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-06-06 18:36:20 -0500

    Remove comments in generatePalette.cjs

--- scripts/generatePalette.cjs
+++ scripts/generatePalette.cjs
@@ -9,11 +9,9 @@ import("nbt").then(module => {
          return console.error("Error while decoding NBT data.", error)
       }
       Object.values(data.value.Metadata.value.CPE.value.BlockDefinitions.value).reverse().forEach(blockDefinition => {
-         // console.log(blockDefinition.value.ID2.value)
          const element = [1, 2, 3].map((value) => blockDefinition.value.Fog.value[value])
          element.push(blockDefinition.value.BlockDraw.value)
          palette.push(element)
-         // palette.push("#" + blockDefinition.value.Fog.value[1].toString(16).padStart(2,0) + blockDefinition.value.Fog.value[2].toString(16).padStart(2,0) + blockDefinition.value.Fog.value[3].toString(16).padStart(2,0))
       })
       fs.writeFileSync("./6-8-5-rgb.json", JSON.stringify(palette))
       console.log("Wrote palette")
