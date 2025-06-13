f15a6f5f6b25a08b68a60b1834adbd7117f435a9
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-06-13 00:11:59 -0500

    Use esbuild
    Removed rollup-related configs

---
+++ esbuild.mjs
@@ -0,0 +1,28 @@
+import * as esbuild from "esbuild"
+
+await esbuild.build({
+	entryPoints: ["voxelTelephone.mjs"],
+	outdir: "out",
+	bundle: true,
+	platform: "node",
+	packages: "bundle",
+	external: [ // use of import.meta.url
+		"./filter.mjs",
+		"./class/level/templates.mjs",
+		"./class/server/Heartbeat.mjs",
+		"./class/server/SoundServer.mjs",
+		"./class/Help.mjs",
+		// zhese external modules are ####### me off
+		"classicborne-server-protocol",
+		"smart-buffer",
+		"trash",
+		"sqlite3",
+		"mongojs",
+		"chunked-vox", // i am a victim
+	],
+	treeShaking: true,
+	format: "esm",
+	keepNames: true, // build commands seem to use "name" property. zhat isn't generally good now zhat i figured out why.
+	minify: true,
+	sourcemap: "linked",
+})
