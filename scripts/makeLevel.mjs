f56a3cf6c488cdeaffcbb8fd40fb44dc520e4333
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-06-28 03:00:29 -0500

    Replace remaining mongojs imports

--- scripts/makeLevel.mjs
+++ scripts/makeLevel.mjs
@@ -1,13 +1,14 @@
 // Creates games to populate the server with playable games.
 
 import serverConfiguration from "../config.json" with { type: "json" }
-import mongojs from "mongojs"
-const gameCollection = mongojs(serverConfiguration.dbName).collection("voxelTelephone")
+import { MongoClient, ObjectId } from "mongodb"
+const client = new MongoClient(serverConfiguration.dbConnectionString ?? "mongodb://localhost:27017")
+const gameCollection = client(serverConfiguration.dbName).collection("voxelTelephone")
 
 function createNewGame(startingSentence, username) {
 	return new Promise((resolve) => {
-		const gameId = new mongojs.ObjectID()
-		const gameNextId = new mongojs.ObjectID()
+		const gameId = new ObjectId()
+		const gameNextId = new ObjectId()
 		const document = {
 			_id: gameId,
 			root: gameId,
