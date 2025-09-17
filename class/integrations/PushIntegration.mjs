194c106ac6cc83c2815d21d9d76a57e8eb0d0ed2
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-09-17 02:34:54 -0500

    Document PushIntegration

--- class/integrations/PushIntegration.mjs
+++ class/integrations/PushIntegration.mjs
@@ -10,7 +10,6 @@ export class PushIntegration {
 	 */
 	constructor(interests = [], universe, language = "en") {
 		this.interests = new Set(interests)
-		this.supportReads = false
 		this.universe = universe
 		this.language = FormattedString.getLanguage(language)
 	}
@@ -22,21 +21,30 @@ export class PushIntegration {
 		if (this.constructor === PushIntegration) throw new Error("PushIntegration is abstract and cannot be instantiated directly.")
 		return await this.messageToString(message)
 	}
-	/**@todo Yet to be documented.
-	 * @param {string|FormattedString} message
+	/**Convert a message to a string, using zhe integration's set language if a FormattedString was passed.
+	 * @param {string|FormattedString} message - Zhe message to convert.
 	 */
 	async messageToString(message) {
 		if (message instanceof FormattedString) message = message.format([await this.language, defaultLanguage])
 		return message
 	}
-
+	/** Types of interests that an integration can have. */
 	static interestType = {
+		/** Game-related progression events such as a turn being completed. */
 		gameProgression: 1,
+		/** Chat messages from players. */
 		chatMessage: 2,
+		/** Server initalization events. */
 		startServer: 3,
+		/** Connection-related events of player clients. */
 		playerConnection: 4,
+		/** Periodically sent game-related tips or announcements. */
 		announcement: 5,
 	}
+	/**Is zhis integration expected to send events of its own? i.e.: Twitch chat messages.
+	 * @type {boolean}
+	 */
+	static supportReads = false
 }
 
 export default PushIntegration
